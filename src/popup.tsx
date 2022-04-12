d92bd84fa6756e32d1d693831f3c9043f3c0ce8d
Kushagra Pathak <201801005@daiict.ac.in>
2022-04-12 19:48:56 +0530

    tailwindcss integrated for easy development

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import './index.css';
 
 function ExtensionContainer() {
 	return (
-		<div>
+		<div className='h-128 w-128 bg-yellow-200'>
 			<h1>Hey there</h1>
 		</div>
 	);
