df17cbb1e85225c5a0e9cadfabe2df92ed3a5aac
Kushagra Pathak <201801005@daiict.ac.in>
2022-04-12 23:42:28 +0530

    webpack issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-// import { CSSTransition } from 'react-transition-group';
 
-export default function App() {
+function App() {
 	return (
-		// <CSSTransition unmountOnExit classNames='fade' timeout={300}>
 		<div style={{ width: 200, height: 500 }} className='overflow-scroll p-2'>
 			<p className='text-xl font-bold'>Extension body</p>
 		</div>
-		// </CSSTransition>
 	);
 }
+
+export default React.memo(App);
