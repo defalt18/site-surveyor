afd4db707168152e0c98c49166e9d258844d3a35
Kushagra Pathak <201801005@daiict.ac.in>
2022-04-13 12:16:34 +0530

    dom structure fixes

--- src/modules/home/views/index.tsx
+++ src/modules/home/views/index.tsx
@@ -8,7 +8,7 @@ import { SkeletonSection } from '../../../molecules/skeleton';
 export default function Home() {
 	const navigation = useNavigate();
 	return (
-		<div className='flex flex-col gap-y-[1.4rem]'>
+		<div className='flex flex-col gap-y-[1.4rem] pb-[1.4rem]'>
 			{map(keys(FEATURES), (key) => (
 				<SkeletonSection
 					key={key}
@@ -17,13 +17,12 @@ export default function Home() {
 					titleClassName='text-primary mb-[2.6rem] text-dark-primary'
 				>
 					<div className='grid grid-cols-2 gap-x-[0.9rem] gap-y-[0.8rem]'>
-						{map(FEATURES[key].data, (item, index) => {
+						{map(FEATURES[key].data, ({ disabled = true, ...item }, index) => {
 							const Icon = item.icon ?? null;
-							const isDisabled = item.disabled ?? true;
 							return (
 								<button
 									onClick={() => navigation(item.path, { state: { details: '/' } })}
-									disabled={isDisabled}
+									disabled={disabled}
 									key={index}
 									className={c(
 										'rounded-lg h-[10.2rem] text-primary text-white flex items-center',
