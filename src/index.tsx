56fdd76c5f8f45656fe51534c40df1d4b5791453
Kushagra Pathak <201801005@daiict.ac.in>
2022-04-13 11:52:30 +0530

    new API changes merged

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
-import { render } from 'react-dom';
 import './index.css';
 import App from './App';
+import { createRoot } from 'react-dom/client';
 
 function ExtensionContainer() {
 	return <App />;
 }
 
-render(<ExtensionContainer />, document.getElementById('root'));
+const rootContainer = document.getElementById('root');
+
+// New API root strategy
+createRoot(rootContainer).render(<ExtensionContainer />);
