5f8230ff996186d555e3c69eecbd2fa0005352ce
Kushagra Pathak <201801005@daiict.ac.in>
2022-05-02 02:17:24 +0530

    improved header

--- src/context/HeaderContext.tsx
+++ src/context/HeaderContext.tsx
@@ -1,14 +1,22 @@
-import React, { SetStateAction, Dispatch, useEffect } from 'react';
-import { noop } from 'lodash';
+import React, { SetStateAction, Dispatch, useEffect, useCallback } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 export const HeaderContext = React.createContext<
 	{ setBackFn: Dispatch<SetStateAction<() => void>> } | undefined
 >(undefined);
 
 export function useHeaderContext(props?: { onBack: () => void }) {
 	const { onBack } = props;
+	const location = useLocation();
+	const navigate = useNavigate();
 	const { setBackFn } = React.useContext(HeaderContext);
+	const DEFAULT_VALUE = useCallback(
+		// @ts-ignore
+		() => navigate(location.state?.details),
+		// @ts-ignore
+		[location.state?.details, navigate]
+	);
 	useEffect(() => {
 		setBackFn(() => onBack);
-		return () => setBackFn(noop);
-	}, [setBackFn, onBack]);
+		return () => setBackFn(() => DEFAULT_VALUE);
+	}, [setBackFn, onBack, DEFAULT_VALUE]);
 }
